3e738d2f0aab0eb6995b58099ebc6fd4f34b938d
himanshu-sugha <143184981+himanshu-sugha@users.noreply.github.com>
2025-01-23 02:32:04 +0530

    Updated App.js with new routes and components structure

--- src/App.js
+++ src/App.js
@@ -3,14 +3,30 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
 import Navbar from './components/Navbar';
 import ThemeToggle from './components/ThemeToggle';
-import NFTOverview from './components/NFTOverview';
+import Home from './components/Home';
 
-// Market Insights Components
-import NFTMarketAnalytics from './components/nft/nftmarketanalyticsreport';
-import NFTScoresInsights from './components/nft/NftscoreresInsights';
-import NFTHoldersInsights from './components/nft/NFTholdersinsights';
+import NftMarketplace from './components/nftmarketplace';
+import NftMarketplaceAnalytics from './components/nftmarketplaceanalytics';
+import NftMarketplaceHolders from './components/nftmarketplaceholders';
+import MarketplaceTraders from './components/marketplacetraders';
+import MarketplaceWashTraders from './components/marketplacewashtraders';
+import NftMarketplaceOverview from './components/NftMarketplaceOverview';
+import NftCollection from './components/NftCollection';
+import CollectionAnalytics from './components/CollectionAnalytics';
+import CollectionHolders from './components/CollectionHolders';
+import CollectionScores from './components/CollectionScores';
+import CollectionTraders from './components/CollectionTraders';
+import CollectionWashtrade from './components/CollectionWashtrade';
+import CollectionCategories from './components/CollectionCategories';
+import CollectionMetadata from './components/CollectionMetadata';
+import CollectionOverview from './components/CollectionOverview';
+
+// Import NFT insight components from nft folder
 import NFTTradersInsights from './components/nft/nfttradersinsights';
 import NFTWashTradeInsights from './components/nft/nftwashtradeinsights';
+import NFTMarketAnalyticsReport from './components/nft/nftmarketanalyticsreport';
+import NFTHoldersInsights from './components/nft/NFTholdersinsights';
+import NFTScoresInsights from './components/nft/NftscoreresInsights';
 
 const App = () => {
   return (
@@ -18,22 +34,38 @@ const App = () => {
       <Router>
         <div className="min-h-screen transition-colors duration-300 dark:bg-gray-900 bg-gray-50 dark:text-white text-gray-900">
           <Navbar />
-          <div className="fixed top-4 right-4 z-50">
-            <ThemeToggle />
-          </div>
-          <main className="container mx-auto px-4 py-8 pt-20">
+          <main className="container mx-auto px-4 py-8">
             <Routes>
-              <Route path="/" element={<NFTOverview />} />
+              <Route path="/" element={<Home />} />
               
-              {/* Market Insights Routes */}
-              <Route path="/nft-market-analytics" element={<NFTMarketAnalytics />} />
-              <Route path="/nft-score-insights" element={<NFTScoresInsights />} />
-              <Route path="/nft-holders-insights" element={<NFTHoldersInsights />} />
-              <Route path="/nft-traders-insights" element={<NFTTradersInsights />} />
-              <Route path="/nft-wash-trade-insights" element={<NFTWashTradeInsights />} />
-              <Route path="/analytics" element={<NFTMarketAnalytics />} />
+              {/* NFT Marketplace Routes */}
+              <Route path="/nftmarketplaceoverview" element={<NftMarketplaceOverview />} />
+              <Route path="/nftmarketplace" element={<NftMarketplace />} />
+              <Route path="/marketplaceanalytics" element={<NftMarketplaceAnalytics />} />
+              <Route path="/nftmarketplaceholders" element={<NftMarketplaceHolders />} />
+              <Route path="/marketplacetraders" element={<MarketplaceTraders />} />
+              <Route path="/marketplacewashtraders" element={<MarketplaceWashTraders />} />
+              
+              {/* NFT Collection Routes */}
+              <Route path="/nftcollection" element={<NftCollection />} />
+              <Route path="/collectionoverview" element={<CollectionOverview />} />
+              <Route path="/collectionanalytics" element={<CollectionAnalytics />} />
+              <Route path="/collectionholders" element={<CollectionHolders />} />
+              <Route path="/collectionscores" element={<CollectionScores />} />
+              <Route path="/collectiontraders" element={<CollectionTraders />} />
+              <Route path="/collectionwashtrade" element={<CollectionWashtrade />} />
+              <Route path="/collectioncategories" element={<CollectionCategories />} />
+              <Route path="/collectionmetadata" element={<CollectionMetadata />} />
+              
+              {/* NFT Insights Routes */}
+              <Route path="/nfttradersinsights" element={<NFTTradersInsights />} />
+              <Route path="/nftwashtradeinsights" element={<NFTWashTradeInsights />} />
+              <Route path="/nftmarketanalyticsreport" element={<NFTMarketAnalyticsReport />} />
+              <Route path="/nftholdersinsights" element={<NFTHoldersInsights />} />
+              <Route path="/nftscoresinsights" element={<NFTScoresInsights />} />
             </Routes>
           </main>
+          <ThemeToggle />
         </div>
       </Router>
     </ThemeProvider>
